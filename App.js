5606321768e76cfc667b7f194124a58393aabf1a
Njaal Wiik <njaal.wiik@gmail.com>
2020-09-27 20:10:00 +0200

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Coffee</Text>
+      <Text>Headline</Text>
+      <h1>Reuse App</h1>
       <StatusBar style='auto' />
     </View>
   );
